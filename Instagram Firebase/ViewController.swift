9f8fb506dbb3afb3412a7c34f511ff6c025c2bdd
Juansebar <juansebar1991@gmail.com>
2020-08-17 11:40:29 -0400

    Initial Commit

---
+++ Instagram Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Firebase
+//
+//  Created by Juan Ramirez on 8/17/20.
+//  Copyright © 2020 Sebapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
