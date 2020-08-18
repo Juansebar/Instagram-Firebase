07307aa3f3da75ef988e4a8165aef793dd2983c9
Juansebar <juansebar1991@gmail.com>
2020-08-18 17:51:14 -0400

    Added other tabs to app

--- Instagram Firebase/MainTabBarController.swift
+++ Instagram Firebase/MainTabBarController.swift
@@ -31,20 +31,63 @@ class MainTabBarController : UITabBarController {
     }
     
     private func setupViewControllers() {
+        // Home
+        let homeController = UIViewController()
+        homeController.view.backgroundColor = .white
+        let homeNavController = templateNavigationController(root: homeController, unselectedIcon: "home_unselected", selectedIcon: "home_selected")
+        
+        // Search
+        let searchController = UIViewController()
+        searchController.view.backgroundColor = .white
+        let searchNavController = templateNavigationController(root: searchController, unselectedIcon: "search_unselected", selectedIcon: "search_selected")
+        
+        // Camera
+        let cameraController = UIViewController()
+        cameraController.view.backgroundColor = .white
+        let cameraNavController = templateNavigationController(root: cameraController, unselectedIcon: "plus_unselected", selectedIcon: "plus_unselected")
+        
+        // Favorites
+        let favoritesController = UIViewController()
+        favoritesController.view.backgroundColor = .white
+        let favoritesNavController = templateNavigationController(root: favoritesController, unselectedIcon: "like_unselected", selectedIcon: "like_selected")
+        
+        // User Profile
         let layout = UICollectionViewFlowLayout()
         let userProfileController = UserProfileController(collectionViewLayout: layout)
-        userProfileController.view.backgroundColor = .red
+        userProfileController.view.backgroundColor = .white
         
-        let navigationController = UINavigationController(rootViewController: userProfileController)
-        navigationController.tabBarItem.image = UIImage(named: "profile_unselected")
-        navigationController.tabBarItem.selectedImage =  UIImage(named: "profile_selected")
+        let userProfileNavController = templateNavigationController(root: userProfileController, unselectedIcon: "profile_unselected", selectedIcon: "profile_selected")
         
         tabBar.tintColor = .black
-        viewControllers = [navigationController, UIViewController()]
+        viewControllers = [homeNavController,
+                           searchNavController,
+                           cameraNavController,
+                           favoritesNavController,
+                           userProfileNavController]
+        
+        // Modify tab bar item insets
+        guard let tabBarItems = tabBar.items else { return }
+        for item in tabBarItems {
+            item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        }
+        
     }
     
     func resetViewControllers() {
         setupViewControllers()
+    }
+    
+    private func templateNavigationController(root viewController: UIViewController, unselectedIcon: String?, selectedIcon: String?) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        if let imageName = unselectedIcon {
+            navigationController.tabBarItem.image = UIImage(named: imageName)
+        }
+        if let imageName = selectedIcon {
+            navigationController.tabBarItem.image = UIImage(named: imageName)
+        }
+        
+        return navigationController
     }
     
 }
