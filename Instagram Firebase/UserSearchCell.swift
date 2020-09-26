1450914c3202e27155ab1945988c40c07553de04
Juansebar <juansebar1991@gmail.com>
2020-09-26 17:42:10 -0400

    Implemented UserSearchCell

---
+++ Instagram Firebase/UserSearchCell.swift
@@ -0,0 +1,56 @@
+//
+//  UserSearchCell.swift
+//  Instagram Firebase
+//
+//  Created by Juan Ramirez on 9/26/20.
+//  Copyright © 2020 Sebapps. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    
+    static let cellId = "UserSearchCellCellId"
+    
+    private let profileImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.backgroundColor = .purple
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .init(white: 0, alpha: 0.5)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        addSubview(usernameLabel)
+        addSubview(separatorView)
+        
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        profileImageView.layer.cornerRadius = 50/2
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        usernameLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        separatorView.anchor(top: nil, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
