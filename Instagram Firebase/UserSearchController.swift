24e1bd9de04349eceddf14febf9e60662cd2bedd
Juansebar <juansebar1991@gmail.com>
2020-09-28 11:40:40 -0400

    Added searchBar and implemented delegate method to filter the users from the content typed

--- Instagram Firebase/UserSearchController.swift
+++ Instagram Firebase/UserSearchController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class UserSearchController: UICollectionViewController {
     
-    private let searchBar: UISearchBar = {
+    private var users: [User] = []
+    private var filteredUsers: [User] = []
+    
+    private lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Enter username"
         searchBar.tintColor = .gray
+        searchBar.delegate = self
 //        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
         return searchBar
     }()  // This executes the closure and returns your searchBar
@@ -27,14 +32,37 @@ class UserSearchController: UICollectionViewController {
         collectionView?.backgroundColor = .white
         
         collectionView.register(UserSearchCell.self, forCellWithReuseIdentifier: UserSearchCell.cellId)
+        
+        fetchUsers()
+    }
+    
+    fileprivate func fetchUsers() {
+        let reference = Database.database().reference().child("users")
+        
+        reference.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let dictionaries = snapshot.value as? [String: Any] else { return }
+            
+            dictionaries.forEach { (key, _) in
+                Database.fetchUserWithUID(uid: key) { [unowned self] (user) in
+                    self.users.append(user)
+                    self.filteredUsers.append(user)
+                }
+                
+                self.collectionView.reloadData()
+            }
+        }) { (error) in
+            print("Failed to fetch users: \(error)")
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return filteredUsers.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserSearchCell.cellId, for: indexPath) as? UserSearchCell else { return UICollectionViewCell() }
+        
+        cell.user = filteredUsers[indexPath.item]
         
         return cell
     }
@@ -51,6 +79,24 @@ extension UserSearchController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return .zero
+    }
+    
+}
+
+// MARK: - UserSearchController: UISearchBarDelegate
+
+extension UserSearchController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.count > 0 {
+            filteredUsers = users.filter { (user) -> Bool in
+                return user.username.contains(searchText)
+            }
+        } else {
+            filteredUsers = users
+        }
+        
+        collectionView.reloadData()
     }
     
 }
