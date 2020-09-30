c9fb8eb68bad581019d9c4d8109391e1c4bde163
Juansebar <juansebar1991@gmail.com>
2020-09-30 13:11:54 -0400

    Created custom animation transition classes to simulate presenting camera on instagram

---
+++ Instagram Firebase/CustomLeftAnimationDismisser.swift
@@ -0,0 +1,35 @@
+//
+//  CustomLeftAnimationDismisser.swift
+//  Instagram Firebase
+//
+//  Created by Juan Ramirez on 9/30/20.
+//  Copyright © 2020 Sebapps. All rights reserved.
+//
+
+import UIKit
+
+class CustomLeftAnimationDismisser: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        
+        guard let fromView = transitionContext.view(forKey: .from) else { return }
+        guard let toView = transitionContext.view(forKey: .to) else { return }
+        
+        containerView.addSubview(toView)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            fromView.frame = CGRect(x: -fromView.frame.width/4, y: 0, width: fromView.frame.width, height: fromView.frame.height)
+            fromView.alpha = 0
+            
+            toView.frame = CGRect(x: 0, y: 0, width: toView.frame.width, height: toView.frame.height)
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
+    }
+    
+}
